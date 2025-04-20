2b02b5d8778559bad8d5c8f477f7c1e85587b28b
ssive7b <33180375+ssive7b@users.noreply.github.com>
2025-04-20 18:17:31 +0200

    bug fix, program now correctly handles  and the likes

--- src/executioner/exec_mode_handlers.c
+++ src/executioner/exec_mode_handlers.c
@@ -27,7 +27,7 @@ void	*exec_command(t_shell *sh, t_anode *node)
 {
 	pid_t	cpid;
 
-	if (!node || !node->args || !node->args[0])
+	if (!node || !node->args || !node->args[0] || node->args[0][0] == '\0')
 		return (update_exit_code(sh, node), NULL);
 	if (node->redirs)
 	{
